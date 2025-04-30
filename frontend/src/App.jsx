e65653f4e97f11a1366f136d9942af55a7c9fb74
pocscz06 <158521706+pocscz06@users.noreply.github.com>
2025-04-30 11:17:04 -0400

    Global display/styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
-import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Game from "./Game";
+import Login from "./Login";
+import Register from "./Register";
+import AdminDashboard from "./components/Dashboard";
+import "./styles/Admin.css";
 
 function App() {
   return (
-    <>
-      <div>Test</div>
-    </>
+    <Router>
+      <div className="app">
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Navigate to="/game" replace />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
